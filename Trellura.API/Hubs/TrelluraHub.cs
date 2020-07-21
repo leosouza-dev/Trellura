5ca12e7eccd13f7df7b67b42cc2e8156b44787e4
sarival <sarival@sarival.com.br>
2020-07-21 11:29:53 -0300

    trantando erros de concorrencia e add método para entrar e enviando o nome

--- Trellura.API/Hubs/TrelluraHub.cs
+++ Trellura.API/Hubs/TrelluraHub.cs
@@ -18,34 +18,67 @@ namespace Trellura.API.Hubs
             _context = context;
         }
 
+        public async Task Entrar(string usuario)
+        {
+            await Clients.All.SendAsync("atualizarListaUsuario", usuario);
+        }
+
         public async Task ObterTodosCards()
         {
-            var cards = await _context.Cards.ToListAsync();
-            await Clients.All.SendAsync("receberTodosCards", cards);
+            try
+            {
+                var cards = await _context.Cards.ToListAsync();
+                await Clients.All.SendAsync("receberTodosCards", cards);
+            }
+            catch (Exception)
+            {
+                await Clients.Caller.SendAsync("exibeMensagemErro", "Não foi recuperar os cards, tente novamente!");
+            }
         }
 
         // cria um card - será se vou precisar deserializar????
         public async Task CriarCard(Card card)
         {
-            _context.Cards.Add(card);
-            await _context.SaveChangesAsync();
-            // recuperar cards e envia no sendAsync? ou no cliente chama chama novamente o ObterCards? ou manda só o novocard?
-            await Clients.All.SendAsync("atualizarBoard", card);
+            try
+            {
+                _context.Cards.Add(card);
+                await _context.SaveChangesAsync();
+                // recuperar cards e envia no sendAsync? ou no cliente chama chama novamente o ObterCards? ou manda só o novocard?
+                await Clients.All.SendAsync("atualizarBoard", card);
+            }
+            catch (Exception)
+            { 
+                await Clients.Caller.SendAsync("exibeMensagemErro", "Não foi possível criar o Card");
+            }
         }
 
         // update (recebe id + card?)
         public async Task AtualizaCard(Card card)
         {
-            _context.Update(card);
-            await _context.SaveChangesAsync();
-            await Clients.All.SendAsync("atualizarBoard", card);
+            try
+            {
+                _context.Update(card);
+                await _context.SaveChangesAsync();
+                await Clients.All.SendAsync("atualizarBoard", card);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                await Clients.Caller.SendAsync("exibeMensagemErro", "Não foi possível atualizar o Card");
+            }
         }
 
         public async Task ApagarCard(Card card)
         {
-            _context.Remove(card);
-            await _context.SaveChangesAsync();
-            await Clients.All.SendAsync("atualizarBoard", card);
+            try
+            {
+                _context.Remove(card);
+                await _context.SaveChangesAsync();
+                await Clients.All.SendAsync("atualizarBoard", card);
+            }
+            catch (Exception)
+            {
+                await Clients.Caller.SendAsync("exibeMensagemErro", "Não foi possível apagar o Card");
+            }
         }
     }
 }
